7ccf63a747ac3f3634addd2dfeca5b49835b92b7
warunkumar <waruntech@gmail.com>
2022-05-08 07:34:19 +0530

    Added by eclipse

---
+++ src/main/java/com/test1/Test2.java
@@ -0,0 +1,20 @@
+package com.test1;
+
+import java.util.*;
+
+public class Test2 {
+
+    public static void main(String[] args){
+
+
+        Map<String, String> map=new HashMap<>();
+        map.put("India","one");
+        map.put("Aus","two");
+        map.put("US","three");
+
+    }
+
+
+}
+
+
